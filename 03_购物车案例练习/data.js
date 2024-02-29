3ed8cb9268113d3df9a15b74a61808eec410e791
xiaoniu <270146073@qq.com>
2024-02-29 16:35:44 +0800

    test_购物车案例

---
+++ 03_购物车案例练习/data.js
@@ -0,0 +1,32 @@
+const books = [
+    {
+        id: 1,
+        name: "西游记",
+        author: "吴承恩",
+        price: 88,
+        count: 1,
+    },
+    {
+        id: 2,
+        name: "红楼梦",
+        author: "曹雪芹",
+        price: 80,
+        count: 1,
+    },
+    {
+        id: 3,
+        name: "三国演义",
+        author: "罗贯中",
+
+        price: 99,
+        count: 1,
+    },
+    {
+        id: 4,
+        name: "水浒传",
+        author: "施耐庵",
+        price: 100,
+        count: 1,
+    }
+
+]
