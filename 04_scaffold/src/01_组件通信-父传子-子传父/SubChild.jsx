a84d4a402f7a0d581fcd1e552a0d4736fe0ca28b
xiaoniu <270146073@qq.com>
2024-03-02 11:43:43 +0800

    父子通信

--- 04_scaffold/src/01_组件通信-父传子-子传父/SubChild.jsx
+++ 04_scaffold/src/01_组件通信-父传子-子传父/SubChild.jsx
@@ -8,9 +8,9 @@ export class SubChild extends Component {
         return (
             <div>
                 <div>子减少组件</div>
-                <button onClick={() => this.subCount(1)}>1</button>
-                <button onClick={() => this.subCount(5)}>5</button>
-                <button onClick={() => this.subCount(10)}>10</button>
+                <button onClick={() => this.subCount(1)}>-1</button>
+                <button onClick={() => this.subCount(5)}>-5</button>
+                <button onClick={() => this.subCount(10)}>-10</button>
             </div>
 
         )
--- 04_scaffold/src/index.js
+++ 04_scaffold/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../src/01_组件通信-父传子/App';
+import App from '../src/01_组件通信-父传子-子传父/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
