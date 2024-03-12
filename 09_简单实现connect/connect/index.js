ec707f83501e11253fd228165c93e587ffeb9da7
xiaoniu <270146073@qq.com>
2024-03-12 22:04:52 +0800

    test_简单实现connect函数

---
+++ 09_简单实现connect/connect/index.js
@@ -0,0 +1,21 @@
+// connect的参数
+// 参数一: 函数
+// 参数二：函数
+// 返回值：函数 => 高阶组件
+import { PureCompontent } from 'react'
+import store from '../store'
+function connect(mapStateToProps, mapDispatchToProps) {
+    return function (WrapCompontent) {
+        class NewCompontent extends PureCompontent {
+            constructor(props) {
+                super(props)
+            }
+            render() {
+                const stateObj = mapStateToProps(store.getState())
+                const diapatchObj = mapDispatchToProps(store.dispatch)
+                return <WrapCompontent {...this.props} {...stateObj} {...diapatchObj} />
+            }
+        }
+        return NewCompontent
+    }
+}
